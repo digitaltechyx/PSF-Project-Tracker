5d871b1d64a1700850b8f3c3d2612975f849b9f9
Ahmed <warhammerarchives7651@gmail.com>
2026-03-24 09:18:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "nexustrack-copy2-0749231-5b20c",
+  "appId": "1:966965380253:web:a39bcb2b864a2437c68074",
+  "apiKey": "AIzaSyDmwZN3FYE0oYLPcLcomi7-xb0dwgKBZdo",
+  "authDomain": "nexustrack-copy2-0749231-5b20c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "966965380253"
+};
